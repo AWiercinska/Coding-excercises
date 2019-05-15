344b59d09bcbfbbf320d078beb76b7cb3e53e947
Estera Kot <esterakot@gmail.com>
2019-05-15 09:44:16 +0200

    Update StringsDataStructures.java

--- src/StringsDataStructures.java
+++ src/StringsDataStructures.java
@@ -1,3 +1,4 @@
+// TODO: 1 Implement an algorithm to determine if a string has all unique characters. What if you can not use additional data structures?
 public class StringsDataStructures {
 
     public static void main(String[] args)
